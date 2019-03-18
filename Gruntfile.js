b342c99ca5a467feb710b6b7c0d0e7b1317a1059
wangding <408542507@qq.com>
2019-03-18 18:43:02 +0800

    master

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        files:{
+                  'css/layout.css': 'less/layout.less'
+            
+        }         
+      }               
+    }
+            
+  });
+    grunt.loadNpmTasks('grunt-contrib-less');
+      grunt.registerTask('default', ['less']);
+
+};
